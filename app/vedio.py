ab8fdb83f01735353835428e4be37536cbe45750
xjn <xjn@xjn.local>
2021-02-10 07:26:58 +0800

    V1.0.0

---
+++ app/vedio.py
@@ -0,0 +1,21 @@
+import requests
+import re
+
+headers = {
+    'User-Agent': 'mozilla/5.0 (iphone; cpu iphone os 14_4 like mac os x) applewebkit/605.1.15 (khtml, like gecko) version/14.0.3 mobile/15e148 safari/604.1'
+}
+
+
+def get_video_url(video_url_share):
+    try:
+        video_url_share = re.findall('https.*/', video_url_share)[0]
+        video_url_redirect = requests.get(url=video_url_share, headers=headers).url
+        video_id = re.findall('video/(\d+)/', str(video_url_redirect))[0]
+        video_url_api = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={video_id}'
+        video_url_json = requests.get(url=video_url_api, headers=headers).json()
+        video_url = video_url_json.get('item_list')[0].get('video').get('play_addr').get('url_list')[0]
+        video_url = video_url.replace('playwm', 'play').replace('&ratio=720p', '')
+        video_url_web = requests.get(url=video_url, headers=headers).url
+        return video_url_web
+    except:
+        return None
---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .vedio import get_video_url
+
+
+def home(request):
+    video_url_share = None
+    if request.method == "POST":
+        video_url_share = request.POST.get('video_url_share')
+        video_url_web = get_video_url(video_url_share)
+        if not video_url_web:
+            return render(request, 'home.html', locals())
+        return redirect(video_url_web)
+    return render(request, 'home.html', locals())
