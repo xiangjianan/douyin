be6b11ddef21b2c008210d86ee03d8ff0512a67b
xjn <xjn@xjn.local>
2021-02-10 22:06:06 +0800

    V1.1.18 防止多次解析

--- static/contact.js
+++ static/contact.js
@@ -4,4 +4,7 @@ $('#content').click(function () {
 $('h1').click(function () {
     alert('不建议解析长视频，本人的乞丐版服务器限速感人');
 });
+$('#get-url').click(function () {
+    $(this).css('pointer-events', 'none');
+});
 console.log("一起聊聊编程、聊聊梦想，我的微信号：inMyLife2021");
