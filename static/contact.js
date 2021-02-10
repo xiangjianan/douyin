8acfb39a619c4aeff9e443ad5d21522bf4604dfc
xjn <xjn@xjn.local>
2021-02-10 21:52:45 +0800

    V1.1.17 新增alert

--- static/contact.js
+++ static/contact.js
@@ -1,4 +1,7 @@
 $('#content').click(function () {
     $('#content').val('');
 });
+$('h1').click(function () {
+    alert('不建议解析长视频，本人的乞丐版服务器限速感人');
+});
 console.log("一起聊聊编程、聊聊梦想，我的微信号：inMyLife2021");
