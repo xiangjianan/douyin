eef93d04e7c0dd65863598619579c4e3d7996b40
xjn <xjn@xjn.local>
2021-02-10 23:52:01 +0800

    V1.1.20 优化代码

--- app/vedio.py
+++ app/vedio.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from douyin import settings
 
 headers = {
     'User-Agent': 'mozilla/5.0 (iphone; cpu iphone os 14_4 like mac os x) applewebkit/605.1.15 (khtml, like gecko) version/14.0.3 mobile/15e148 safari/604.1'
@@ -7,10 +8,14 @@ headers = {
 
 
 def get_video_url(video_url_share):
+    """
+    :param video_url_share: 分享链接
+    :return: 视频源链接, 视频描述
+    """
     try:
         video_url_share = re.findall('https.*/', video_url_share)[0]
         video_url_redirect = requests.get(url=video_url_share, headers=headers).url
-        video_id = re.findall('video/(\d+)/', str(video_url_redirect))[0]
+        video_id = re.findall(r'video/(\d+)/', str(video_url_redirect))[0]
         video_url_api = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={video_id}'
         video_url_json = requests.get(url=video_url_api, headers=headers).json()
         video_url = video_url_json.get('item_list')[0].get('video').get('play_addr').get('url_list')[0]
@@ -19,9 +24,11 @@ def get_video_url(video_url_share):
 
         video_title = video_url_json.get('item_list')[0].get('desc')
         video_content = requests.get(url=video_url_web, headers=headers).content
-        with open(f'/root/static/douyin_static/video.mp4', 'wb')as f:
+        with open(settings.LOCALE_DIR, 'wb') as f:
             f.write(video_content)
 
         return video_url_web, video_title
-    except:
+
+    except Exception as e:
+        print(e)
         return None, None
--- douyin/settings.py
+++ douyin/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 STATIC_ROOT = '/root/static/douyin_static/'
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'douyin.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -116,7 +111,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -124,3 +118,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+LOCALE_DIR = '/root/static/douyin_static/video.mp4'
