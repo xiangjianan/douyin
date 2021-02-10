e12070d9f16326f29be871b47d006fbbea94e2e8
xjn <xjn@xjn.local>
2021-02-10 22:13:17 +0800

    V1.1.19 防止多次解析,新增定时器

--- static/contact.js
+++ static/contact.js
@@ -6,5 +6,8 @@ $('h1').click(function () {
 });
 $('#get-url').click(function () {
     $(this).css('pointer-events', 'none');
+    setTimeout(function () {
+        $('#get-url').css('pointer-events', 'auto');
+    }, 3000);
 });
 console.log("一起聊聊编程、聊聊梦想，我的微信号：inMyLife2021");
