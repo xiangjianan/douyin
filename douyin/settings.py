ba01564e8c8be2957804aadb75a0a55db3c3e313
xjn <xjn@xjn.local>
2021-02-10 08:33:49 +0800

    V1.0.3

--- douyin/settings.py
+++ douyin/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
--- douyin/urls.py
+++ douyin/urls.py
@@ -19,5 +19,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
     path('douyin/', views.home),
 ]
