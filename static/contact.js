28273195fd93627caeb189bec68ed158190994ed
xjn <xjn@xjn.local>
2021-02-11 16:17:24 +0800

    V1.2.1 优化使用体验

--- static/contact.js
+++ static/contact.js
@@ -7,40 +7,42 @@ $('#title').click(function () {
 2、长视频（请耐心等待）
 `);
 });
-$('#get-url').click(function () {
-    $(this).css('pointer-events', 'none');
+$('#download').click(function () {
+    $('#watch').css('pointer-events', 'none');
+    $('#download').css('pointer-events', 'none');
     setTimeout(function () {
-        $('#get-url').css('pointer-events', 'auto');
+        $('#watch').css('pointer-events', 'auto');
+        $('#download').css('pointer-events', 'auto');
     }, 3000);
 });
 
 $('#watch').click(function () {
-  $('#watch').css('pointer-events', 'none');
-  setTimeout(function () {
-    $('#watch').css('pointer-events', 'auto');
-  }, 3000);
-  let content = $("#content").val().trim();
-  $.ajax({
-    type: "POST",
-    dataType: "text",
-    url: 'http://8.130.48.251:8088/api/douyin',
-    data: {
-      "video_url_share": content,
-    },
-    success: function (msg) {
-      msg = JSON.parse(msg)
-      if (!msg.error) {
-        let video_url = msg.data;
-        $("#content").val('');
-        $('#video_url').attr("href", video_url);
-        document.getElementById("video_url").click();
-      }
-      else {
-        $('#error').text('链接无效');
+    $('#watch').css('pointer-events', 'none');
+    $('#download').css('pointer-events', 'none');
+    setTimeout(function () {
         $('#watch').css('pointer-events', 'auto');
-      }
-    },
-  });
+        $('#download').css('pointer-events', 'auto');
+    }, 3000);
+    let content = $("#content").val().trim();
+    $.ajax({
+        type: "POST",
+        dataType: "text",
+        url: 'http://8.130.48.251:8088/api/douyin',
+        data: {
+            "video_url_share": content,
+        },
+        success: function (msg) {
+            msg = JSON.parse(msg)
+            if (!msg.error) {
+                let video_url = msg.data;
+                $('#video_url').attr("href", video_url);
+                document.getElementById("video_url").click();
+            } else {
+                $('#error').text('链接无效');
+                $('#watch').css('pointer-events', 'auto');
+            }
+        },
+    });
 });
 
 console.log("一起聊聊编程、聊聊梦想，我的微信号：inMyLife2021");
