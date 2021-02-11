a1951d83f5237619c88af503b6d01f6d37063444
xjn <xjn@xjn.local>
2021-02-11 18:06:40 +0800

    V1.3.1 requests优化

--- app/vedio.py
+++ app/vedio.py
@@ -14,16 +14,16 @@ def get_video_url(video_url_share):
     """
     try:
         video_url_share = re.findall('https.*/', video_url_share)[0]
-        video_url_redirect = requests.get(url=video_url_share, headers=headers).url
+        video_url_redirect = get_response(video_url_share).url
         video_id = re.findall(r'video/(\d+)/', str(video_url_redirect))[0]
         video_url_api = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={video_id}'
-        video_url_json = requests.get(url=video_url_api, headers=headers).json()
+        video_url_json = get_response(video_url_api).json()
         video_url = video_url_json.get('item_list')[0].get('video').get('play_addr').get('url_list')[0]
         video_url = video_url.replace('playwm', 'play').replace('&ratio=720p', '')
-        video_url_web = requests.get(url=video_url, headers=headers).url
+        video_url_web = get_response(video_url).url
 
         video_title = video_url_json.get('item_list')[0].get('desc')
-        video_content = requests.get(url=video_url_web, headers=headers).content
+        video_content = get_response(video_url_web).content
         with open(settings.LOCALE_DIR, 'wb') as f:
             f.write(video_content)
 
@@ -32,3 +32,17 @@ def get_video_url(video_url_share):
     except Exception as e:
         print(e)
         return None, None
+
+
+def get_response(url):
+    try:
+        response = requests.get(url=url, headers=headers, timeout=5)
+        if response.status_code == 200:
+            return response
+    except Exception as e:
+        print(e)
+        for i in range(1, 10):
+            print(f'请求{url}超时，第{i}次重复请求')
+            response = requests.get(url, headers=headers, timeout=5)
+            if response.status_code == 200:
+                return response
