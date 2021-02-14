6271c9fec93ffd366c73992bee53afb9494e94fd
xjn <xjn@xjn.local>
2021-02-15 03:11:58 +0800

    V1.4.5 解决相同视频重复下载的问题

--- app/models.py
+++ app/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class Video(models.Model):
     video_name = models.CharField(max_length=32)
-    video_id = models.CharField(max_length=64)
+    video_id = models.CharField(max_length=32)
