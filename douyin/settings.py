934b144e2657fd713c46859074ffb2dae0405ac5
xjn <xjn@xjn.local>
2021-02-28 02:53:51 +0800

    V2.0.1 新增视频解析API

--- douyin/settings.py
+++ douyin/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-    'app_api.apps.AppConfig',
+    'app_api.apps.AppApiConfig',
     'rest_framework',
 ]
 
