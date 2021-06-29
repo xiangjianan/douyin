906dea7fd8830a0d0ded8ecdda4abc2954b4b2d6
xjn <xjn@xjndeMacBook-Air.local>
2021-06-30 01:42:45 +0800

    V2.8.3 优化form表单回车提交

--- app/views.py
+++ app/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
-from utils.vedio import get_video_url
-from utils.my_log import logger
 
 
 def home(request):
-    video_url_share = None
-    if request.method == "POST":
-        video_url_share = request.POST.get('video_url_share')
-        video_url_web, video_name = get_video_url(video_url_share)
-        if not video_url_web:
-            return render(request, 'home.html', locals())
-        logger.info(f'{video_url_web} {video_name}')
-        return render(request, 'download.html', locals())
-    return render(request, 'home.html', locals())
+    return render(request, 'home.html')
 
 
 def download(request):
