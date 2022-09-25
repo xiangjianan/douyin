58cb27102699ea256f0b4d8192b1cdaa661d28a9
xjn <xjn@xjndeMacBook-Air.local>
2022-09-25 14:16:09 +0800

    V2.8.7 github

--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
+def auth(func):
+    def wrapper(request, *args, **kwargs):
+        key = request.GET.get('from')
+        print(key)
+        if key != 'github':
+            return render(request, 'home-auth.html')
+        res = func(request, *args, **kwargs)
+        return res
+    return wrapper
+
+
+@auth
 def home(request):
     return render(request, 'home.html')
 
 
+@auth
 def download(request):
     video_url_web = request.GET.get('video_url_web')
     video_name = request.GET.get('video_name')
